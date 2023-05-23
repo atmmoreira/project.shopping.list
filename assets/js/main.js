c3e356138058417a4dd8eb5053dd4c0a7aa92b0f
Anderson Martins <atmmoreira.rj@gmail.com>
2023-05-23 09:28:50 -0300

    feature: remove single item and clearbtn items

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 const itemForm = document.getElementById('itemForm');
 const inputTextItem = document.getElementById('inputTextItem');
 const itemList = document.getElementById('itemList');
+const btnClear = document.getElementById('btnClearItem');
 
 // Add Item
-function addItem (e) {
+function addItem(e) {
   e.preventDefault();
 
   // NewItem
@@ -19,8 +20,8 @@ function addItem (e) {
   const li = createElement('li', 'list-group-item d-flex justify-content-between align-items-start');
   const firstDiv = createElement('div', 'ms-2 me-auto');
   const secondDiv = createElement('div', 'fw-bold');
-  const spanDelete = createElement('span', 'btnDelete text-danger'); 
-  const iconDelete = createElement('i', 'fa-regular fa-trash-can');
+  const spanDelete = createElement('span', 'text-danger');
+  const iconDelete = createElement('i', 'btnDelete fa-regular fa-trash-can');
 
   // Append Elements
   li.appendChild(firstDiv);
@@ -34,12 +35,25 @@ function addItem (e) {
   inputTextItem.value = '';
 }
 
-function createElement (element, className) {
+function createElement(element, className) {
   const elementItem = document.createElement(element);
   elementItem.className = className;
   return elementItem;
 }
 
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains('btnDelete')) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+}
 
 //  Add Eventlistener
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+btnClear.addEventListener('click', clearItems);
