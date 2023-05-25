b1cce007d61011dea7b17a9bb0c6f42fbbdbf0b7
Anderson Martins <atmmoreira.rj@gmail.com>
2023-05-25 09:33:03 -0300

    feature: add items to localstorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,18 +6,25 @@ const inputTextFilter = document.getElementById('inputTextFilter');
 const hrLine = document.getElementById('hrLine');
 
 // Add Item
-function addItem(e) {
+function onAddItemSubmit(e) {
   e.preventDefault();
-
   // NewItem
   const newItem = inputTextItem.value;
-
   // Validate Input
   if (newItem.trim() === '') {
     alert('Por favor, adicione um item');
     return;
   }
-
+  // Add item to the DOM
+  addItemToDOM(newItem);
+  // Add item to localstorage
+  addItemToStorage(newItem);
+  checkUI();
+  // Clear Value Input Field
+  inputTextItem.value = '';
+}
+// Add Item to the DOM
+function addItemToDOM(item) {
   // Create Elements
   const li = createElement('li', 'list-group-item d-flex justify-content-between align-items-start');
   const firstDiv = createElement('div', 'ms-2 me-auto');
@@ -30,25 +37,35 @@ function addItem(e) {
 
   // Append Elements
   li.appendChild(firstDiv);
-  secondDiv.appendChild(document.createTextNode(newItem));
+  secondDiv.appendChild(document.createTextNode(item));
   firstDiv.appendChild(secondDiv);
   spanDelete.appendChild(iconDelete);
   li.appendChild(spanDelete);
 
   // Add li to the DOM
   itemList.appendChild(li);
-  checkUI();
-
-  // Clear Value Input Field
-  inputTextItem.value = '';
 }
+// Add item to localstorage
+function addItemToStorage(item) {
+  let itemsFromStorage;
 
+  if (localStorage.getItem('items') === null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+  }
+  // Add new items to array
+  itemsFromStorage.push(item);
+  // Convert to JSON string and set to localstorage
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+}
+// Create Element
 function createElement(element, className) {
   const elementItem = document.createElement(element);
   elementItem.className = className;
   return elementItem;
 }
-
+// Remove Item to the DOM
 function removeItem(e) {
   if (e.target.classList.contains('btnDelete')) {
     if (confirm('Tem certeza que deseja excluir?')) {
@@ -57,14 +74,14 @@ function removeItem(e) {
   }
   checkUI();
 }
-
+// Clear Items
 function clearItems() {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
   checkUI();
 }
-
+// Check UI Elements
 function checkUI() {
   const itemsUI = document.querySelectorAll('li');
   if (itemsUI.length === 0) {
@@ -77,11 +94,27 @@ function checkUI() {
     btnClear.className = 'd-block btn btn-warning mb-3 w-100';
   }
 }
+// Filter Items to the DOM
+function filterItems(e) {
+  const itemsUI = document.querySelectorAll('li');
+  const text = e.target.value.toLowerCase();
+
+  itemsUI.forEach(item => {
+    const itemName = item.firstElementChild.innerText.toLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.className = 'list-group-item d-flex justify-content-between align-items-start';
+    } else {
+      item.className = 'd-none';
+    }
+  })
+}
 
 //  Add Eventlistener
-itemForm.addEventListener('submit', addItem);
+itemForm.addEventListener('submit', onAddItemSubmit);
 itemList.addEventListener('click', removeItem);
 btnClear.addEventListener('click', clearItems);
+inputTextFilter.addEventListener('input', filterItems);
 
 // Invocate Functions
 checkUI();
